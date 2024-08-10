f5b5ca96c16914fd63e29ab805f5b4fb1241d6cc
Binay Rajbanshi <binay@squarebx.com>
2024-08-10 18:53:55 +0545

    schema changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany()
+  const posts = await db.query.images.findMany()
   console.log(posts)
 
   const dummyData = [
